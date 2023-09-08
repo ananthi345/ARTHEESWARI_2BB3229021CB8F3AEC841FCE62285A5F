1db590737c02e9f4c4d4f420c99175b07eee466a
ananthi345 <144431750+ananthi345@users.noreply.github.com>
2023-09-08 14:15:45 +0530

    Add files via upload

---
+++ UNIT 1-CHALLENGE/CHALLENGE 1.py
@@ -0,0 +1,7 @@
+my_num = int(input("Enter a number :"))
+my_factorial = 1
+while(my_num>0):
+   my_factorial = my_factorial*my_num
+   my_num=my_num-1
+print("The factorial of the number is : ")
+print(my_factorial)
